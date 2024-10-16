3daee1474e305d15388c9872c9df1b024dde76d6
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2024-10-16 18:13:25 +0530

    "Add Backand In AccountSettingStudent.kt"

--- app/src/main/java/com/example/eduexamine/AccountSettingStudent.kt
+++ app/src/main/java/com/example/eduexamine/AccountSettingStudent.kt
@@ -1,20 +1,32 @@
 package com.example.eduexamine
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.eduexamine.databinding.ActivityAccountSettingStudentBinding
 
 class AccountSettingStudent : AppCompatActivity() {
+    private val binding:ActivityAccountSettingStudentBinding by lazy {
+        ActivityAccountSettingStudentBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_account_setting_student)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.button8.setOnClickListener{
+            val intent=Intent(this,UpdatePassStudent::class.java)
+            startActivity(intent)
+            finish()
+            Toast.makeText(this,"Update Your Password",Toast.LENGTH_SHORT).show()
         }
     }
 }
