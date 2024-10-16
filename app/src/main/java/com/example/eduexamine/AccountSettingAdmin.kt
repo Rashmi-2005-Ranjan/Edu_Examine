89a91876d7fbd8588a2abdb42ac1a4bf3421142c
kalinga kishore parida <kalingakishore2003@gmail.com>
2024-10-16 19:05:48 +0530

    "Design Admin Delete Account Page"

--- app/src/main/java/com/example/eduexamine/AccountSettingAdmin.kt
+++ app/src/main/java/com/example/eduexamine/AccountSettingAdmin.kt
@@ -1,20 +1,39 @@
 package com.example.eduexamine
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.eduexamine.databinding.ActivityAccountSettingAdminBinding
+import com.example.eduexamine.databinding.ActivityAccountSettingStudentBinding
 
 class AccountSettingAdmin : AppCompatActivity() {
+    private val binding:ActivityAccountSettingAdminBinding by lazy {
+        ActivityAccountSettingAdminBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_account_setting_admin)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.button8.setOnClickListener{
+            val intent=Intent(this,UpdatePassAdmin::class.java)
+            startActivity(intent)
+            finish()
+            Toast.makeText(this,"Update Your Password",Toast.LENGTH_SHORT).show()
+        }
+        binding.button9.setOnClickListener{
+            val intent=Intent(this,DeleteAccountAdmin::class.java)
+            startActivity(intent)
+            finish()
+            Toast.makeText(this,"Delete Your Account",Toast.LENGTH_SHORT).show()
         }
     }
 }
