d6f29e987b6ef8208e93bb8f9681db283d5b4e7d
Rashmi-2005-Ranjan <rbstudu2022@gmail.com>
2024-10-27 20:10:47 +0530

    "Remove Intent From Back To Main Page Button"

--- app/src/main/java/com/example/eduexamine/StudentActivityFragments/ProfileFragment.kt
+++ app/src/main/java/com/example/eduexamine/StudentActivityFragments/ProfileFragment.kt
@@ -1,31 +1,23 @@
 package com.example.eduexamine.StudentActivityFragments
 
-import android.app.AlertDialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
 import com.example.eduexamine.AcademicInfoStudent
 import com.example.eduexamine.AccountSettingStudent
 import com.example.eduexamine.BasicInfoStudent
 import com.example.eduexamine.EducationalDetails
 import com.example.eduexamine.R
 import com.example.eduexamine.SocialStudents
-import com.example.eduexamine.StudentHome
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import de.hdodenhof.circleimageview.CircleImageView
-import java.util.*
 
 class ProfileFragment : Fragment() {
 
@@ -80,8 +72,7 @@ class ProfileFragment : Fragment() {
             startActivity(intent)
         }
         backToMainButton.setOnClickListener{
-            val intent = Intent(requireContext(), StudentHome::class.java)
-            startActivity(intent)
+
         }
 
         // Initialize profile and background ImageViews
