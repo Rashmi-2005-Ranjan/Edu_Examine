18fd0182d216000aadb303acfee87d0000cce977
Biku2004 <bikash.behera24.04.2004@gmail.com>
2024-10-30 21:30:08 +0530

    initialize the Firebase in the mainActivity file

--- app/src/main/java/com/example/eduexamine/MainActivity.kt
+++ app/src/main/java/com/example/eduexamine/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        FirebaseApp.initializeApp(this)
     }
 }
