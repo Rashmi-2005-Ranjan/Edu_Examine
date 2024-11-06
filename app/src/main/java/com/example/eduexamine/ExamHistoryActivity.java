93061e40a9b8b47d6a45c1f50635a88541f34a5d
Satya <satyasarathi0014@gmail.com>
2024-11-06 22:42:37 +0530

    Adding a data class file and adapter file

--- app/src/main/java/com/example/eduexamine/ExamHistoryActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.eduexamine;
-
-import android.app.Activity;
-
-public class ExamHistoryActivity extends Activity {
-}
---
+++ app/src/main/java/com/example/eduexamine/StudentActivityFragments/ExamHistory.kt
@@ -0,0 +1,8 @@
+package com.example.eduexamine.StudentActivityFragments
+
+data class ExamHistory(
+    val title: String,
+    val date: String,
+    val score: String
+)
+
