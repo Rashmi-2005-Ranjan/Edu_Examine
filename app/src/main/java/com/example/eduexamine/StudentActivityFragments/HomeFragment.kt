c3da0faf278a7ca8be7bd022cc75d0c4ec260f33
Biswajit Das <biswajit.ontheway@gmail.com>
2024-11-18 01:08:37 +0530

    some code changes and updated in aplly part

--- app/src/main/java/com/example/eduexamine/StudentActivityFragments/HomeFragment.kt
+++ app/src/main/java/com/example/eduexamine/StudentActivityFragments/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.example.eduexamine.StudentActivityFragments
 
-import com.example.eduexamine.StudentActivityFragments.ResultFragment
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
